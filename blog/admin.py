ea929c52c2001d64354411180a89af0b65a3016c
Gabriel Zirkovits <gabriel.zirkovits@edu.campus02.at>
2022-07-11 20:45:31 +0200

    Registered BlogPost model to admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import BlogPost
 
 # Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date')
+    ordering = ('-date',)
+    
+    class Meta:
+        verbose_name_plural = 'Blogpost'
+        
+
+admin.site.register(BlogPost, BlogPostAdmin)
